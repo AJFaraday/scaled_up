debe8a51b257a3d38a9cd122ca9eec6e15270b5e
ajfaraday <andrewfaraday@hotmail.co.uk>
2014-04-29 22:53:10 +0100

    refactored initial note creation process

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,5 +2,30 @@ class Note < ActiveRecord::Base
 
   has_and_belongs_to_many :events
   has_and_belongs_to_many :event_profiles
+
+  NOTE_NAMES = %w{C C# D D# E F F# G G# A A# B}
+
+  def Note.create_initial
+    # midi note A0
+    midi_note = 21
+    octave_index = 9
+    octave = 0
+
+    until midi_note > 108
+      note = NOTE_NAMES[octave_index%12]
+      octave += 1 if note == 'C'
+      puts "note: #{note}#{octave}"
+
+      unless Note.find_by_name("#{note}#{octave}")
+        Note.create(
+          name: "#{note}#{octave}",
+          midi_note: midi_note
+        )
+      end
+
+      octave_index += 1
+      midi_note += 1
+    end
+  end 
  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,5 @@
 # Generate notes (midi notes 21 - 108)
-
-octave_notes = %w{C C# D D# E F F# G G# A A# B}
-
-# midi note A0
-midi_note = 21
-# Starts on an A
-octave_index = 9
-# starts octave 0
-octave = 0
-
-until midi_note > 108
-  note = octave_notes[octave_index%12]
-  octave += 1 if note == 'C'
-  puts "note: #{note}#{octave}"
-
-  unless Note.find_by_name("#{note}#{octave}")
-    Note.create(
-      name: "#{note}#{octave}",
-      midi_note: midi_note
-    )
-  end
-
-  octave_index += 1
-  midi_note += 1
-end
+Note.create_initial
 
 # Create Sample Groups
 
