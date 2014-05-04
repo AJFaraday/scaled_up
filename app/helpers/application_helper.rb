7d10a6dc79d2e51d16eaa8afef40a242f7090b34
ajfaraday <andrewfaraday@hotmail.co.uk>
2014-05-04 16:06:00 +0100

    Added system settings UI, and added conductors menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,18 @@ module ApplicationHelper
     end 
   end
 
+  def check_local_request
+    unless local_request?
+      flash[:warning] = "This action can only be called from the conductors computer."
+      redirect_to '/'
+    end
+  end
+
+  def local_request?
+    puts request.remote_ip
+    request.remote_ip == '127.0.0.1'
+  end
+
   
  
 end
