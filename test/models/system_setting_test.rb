ea040a79869a263b9e82d5b8aec52eaf24757702
ajfaraday <andrewfaraday@hotmail.co.uk>
2014-05-10 14:05:59 +0100

    fixtures now load without error and first unit test passes...

--- test/models/system_setting_test.rb
+++ test/models/system_setting_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class SystemSettingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def test_get
+    x = SystemSetting.get('quaver_time')
+    puts x.inspect
+    assert x.is_a?(Integer)
+    assert_equal 220, x
+  end
+
 end
