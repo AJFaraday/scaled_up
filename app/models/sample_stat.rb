34692953773ef8aafe904722e569d27856b3c78d
ajfaraday <andrewfaraday@hotmail.co.uk>
2014-05-14 00:18:28 +0100

    Added Sample chart and changed styling for stats page

--- app/models/sample_stat.rb
+++ app/models/sample_stat.rb
@@ -1,3 +1,18 @@
 class SampleStat < ActiveRecord::Base
 
+  def SampleStat.bar_data
+    stats = SampleStat.all.order('name asc')
+    data = {
+      labels: stats.collect{|x|x.name},
+      datasets: [
+        {
+          fillColor: '#FFFFFF',
+          strokeColor: '#000000',
+          data: stats.collect{|x|x.cnt}          
+        }
+      ]
+    }
+    data.to_json
+  end
+
 end
